cfaafcda1d8363bbe2a17e00cbbc3623ebee1d3c
Harriet Ryder <harriet@zuko.io>
2019-03-06 17:25:55 +0000

    Add a DynamoDB table

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
 import json
+import boto3
+import os
 
-print('Loading function')
+# Create client outside of handler for best performance
+
+region_name = os.environ['REGION_NAME']
+dynamo = boto3.client('dynamodb', region_name=region_name)
+table_name = os.environ['TABLE_NAME']
 
 def lambda_handler(event, context):
+    result = dynamo.scan(TableName=table_name)
     return {
             'statusCode': '200',
-            'body': json.dumps('Success!'),
+            'body': json.dumps(result),
             'headers': {'Content-Type': 'application/json'}
             }
